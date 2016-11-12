99871b74652e13411cf4620c12ccfd71be8696d9
Max Holder <mxhold@gmail.com>
2016-11-12 14:49:51 -0800

    Working app

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,6 @@ use std::io::Read;
 use regex::Regex;
 use url::percent_encoding::percent_decode;
 
-fn hello(req: Request, res: Response) {
-    let uri = match req.uri {
-        RequestUri::AbsolutePath(uri) => uri,
-        _ => panic!(),
-    };
-    let re = Regex::new(r"^/(.+)$").unwrap();
-
-    let name = if re.is_match(&uri) {
-        let percent_encoded_name = re.captures(&uri).unwrap().at(1).unwrap();
-        percent_decode(percent_encoded_name.as_bytes()).decode_utf8().unwrap().to_string()
-    } else {
-        "World".to_string()
-    };
-
-    let response_body = "Hello, ".to_string() + &name + "!";
-    res.send(response_body.as_bytes()).unwrap();
-}
-
 struct App {
     hyper_server: hyper::server::Server,
 }
@@ -38,23 +20,36 @@ impl App {
         }
     }
 
-    fn handle<H: hyper::server::Handler + 'static>(self, handler: H) ->
-        hyper::Result<hyper::server::Listening> {
-        self.hyper_server.handle(handler)
+    fn register_route<H>(self, route: String, handler: H) where H: Fn(Request, Response, Option<String>) + std::marker::Sync + std::marker::Send  + 'static {
+        self.hyper_server.handle(move |req: Request, res: Response| {
+            let uri = match req.uri.clone() {
+                RequestUri::AbsolutePath(uri) => uri,
+                _ => panic!(),
+            };
+            let route = "^/".to_string() + &route + "$";
+            let re = Regex::new(&route).unwrap();
+
+            let route_match = if re.is_match(&uri) {
+                let percent_encoded_name = re.captures(&uri).unwrap().at(1).unwrap();
+                Some(percent_decode(percent_encoded_name.as_bytes()).decode_utf8().unwrap().to_string())
+            } else {
+                None
+            };
+
+            handler(req, res, route_match);
+        }).unwrap();
     }
 }
 
 fn main() {
     let app = App::new("0.0.0.0:5000").unwrap();
-    app.handle(hello).unwrap();
-    // TODO:
-    // app.register_route("(.*)", |req, res, name| {
-    //     let response_body = match name {
-    //         Some(name) => "Hello, ".to_string() + &name + "!",
-    //         None => "Hello, World!",
-    //     }
-    //     res.send(response_body.as_bytes()).unwrap();
-    // });
+    app.register_route("(.+)".to_string(), move |_req, res, name| {
+        let response_body = match name {
+            Some(name) => "Hello, ".to_string() + &name + "!",
+            None => "Hello, World!".to_string(),
+        };
+        res.send(response_body.as_bytes()).unwrap();
+    });
 }
 
 #[test]
